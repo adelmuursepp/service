5d5313e6c7704536329dbf9a9e30fa4d8aa04f35
Adel Muursepp <adel.muursepp@gmail.com>
2020-05-10 00:52:04 +0300

    started api

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, request, jsonify, make_response
+from flask_restplus import Api, Resource, fields
+from sklearn.externals import joblib
+import numpy as np
+import sys
+import pickle
+
+flask_app = Flask(__name__)
+app = Api(app = flask_app,
+ version = "1.0",
+ title = "Language Level Predictor",
+ description = "Based on Position Of Speech tags, predict text level")
+
+name_space = app.namespace('prediction', description='Prediction APIs')
+
+model = app.model('Prediction params',
+ {'A': fields.Float(required = True,
+  description="A",
+  help="A cannot be blank"),
+ 'C': fields.Float(required = True,
+  description="C",
+  help="C cannot be blank"),
+ 'D': fields.Float(required = True,
+  description="D",
+  help="D cannot be blank"),
+ 'G': fields.Float(required = True,
+  description="G",
+  help="G cannot be blank"),
+ 'H': fields.Float(required = True,
+  description="H",
+  help="H cannot be blank"),
+ 'I': fields.Float(required = True,
+  description="I",
+  help="I cannot be blank"),
+ 'J': fields.Float(required = True,
+  description="J",
+  help="J cannot be blank"),
+ 'K': fields.Float(required = True,
+  description="K",
+  help="K cannot be blank"),
+ 'N': fields.Float(required = True,
+  description="N",
+  help="N cannot be blank"),
+ 'O': fields.Float(required = True,
+  description="O",
+  help="O cannot be blank"),
+ 'P': fields.Float(required = True,
+  description="P",
+  help="P cannot be blank"),
+ 'S': fields.Float(required = True,
+  description="S",
+  help="S cannot be blank"),
+ 'U': fields.Float(required = True,
+  description="U",
+  help="U cannot be blank"),
+ 'V': fields.Float(required = True,
+  description="V",
+  help="V cannot be blank"),
+ 'X': fields.Float(required = True,
+  description="X",
+  help="X cannot be blank"),
+ 'Y': fields.Float(required = True,
+  description="Y",
+  help="Y cannot be blank"),
+ 'Z': fields.Float(required = True,
+  description="Z",
+  help="Z cannot be blank"),
+  'Z': fields.Float(required = True,
+  description="Z",
+  help="Z cannot be blank")})
+
+
+classifier = pickle.load(open('dtree.sav', 'rb'))
+
+
+@name_space.route("/")
+class MainClass(Resource):
+
+  def options(self):
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+  @app.expect(model)
+  def post(self):
+    try:
+      formData = request.json
+      data = [float(val) for val in formData.values()]
+      print("predicting ...")
+      print('data: ', data)
+      prediction = classifier.predict(np.array(data).reshape(1, -1))
+      print("predicted")
+      print("prediction ", prediction)
+      types = { 0: "A2", 1:'B1', 2:'B2', 3:'C1' }
+      response = jsonify({
+        "statusCode": 200,
+        "status": "Prediction made",
+        "result": "The language level is: " + types[prediction[0]]
+        })
+      response.headers.add('Access-Control-Allow-Origin', '*')
+      return response
+    except Exception as error:
+      return jsonify({
+        "statusCode": 500,
+        "status": "Could not make prediction",
+        "error": str(error)
+        })
+
+      MainClass.post("ghj")
+
---
+++ model_generator.py
@@ -0,0 +1,70 @@
+# Import libraries
+import numpy as np
+print('imported numpy')
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.externals import joblib
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+
+
+print('imported all')
+
+data_table = pd.read_csv('postags_lemmas_levels_data.csv')
+data_table = data_table.drop(['Unnamed: 0','tekstikood', 'filename'], 1)
+
+print('read data')
+
+labelencoder_0 = LabelEncoder() #independent variable encoder
+data_table.iloc[:,17] = labelencoder_0.fit_transform(data_table.iloc[:,17])
+
+#Transforming values into percentages of total and splitting into target and features
+features = data_table.loc[:, "A":"Z"]
+target_var = data_table.loc[:, "keeletase"]
+
+print('split to test and train')
+X_train, X_test, y_train, y_test =\
+    train_test_split(features.loc[:,'A':"Z"], target_var, test_size = 0.5, random_state=1111)
+
+
+
+# # Split the dataset into features and labels
+# X = features
+# y = target_var
+
+# # Split the dataset into training (80%) and testing (20%) data
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, shuffle = True)
+
+# Build the classifier and make prediction
+# classifier = DecisionTreeClassifier()
+# classifier.fit(X_train, y_train)
+# print('fit trainging data')
+# prediction = classifier.predict(X_test)
+
+print('=' * 80)
+print("Decision Tree")
+dt_param_grid = {
+    'max_depth': [5,7, None],
+    'criterion': ['gini', 'entropy']
+}
+dt = DecisionTreeClassifier()
+dt_search = GridSearchCV(dt, dt_param_grid, cv=3, return_train_score=True)
+dt_search.fit(X_train, y_train)
+classifier = dt_search.best_estimator_
+prediction = classifier.predict(X_test)
+
+filename = 'dtree.sav'
+pickle.dump(classifier, open(filename, 'wb'))
+
+
+# Save the model to disk
+# joblib.dump(classifier, 'classifier.joblib')
+
+
+
+
+
